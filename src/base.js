5e30011f6f47992487c0008a9ac4041affb52192
nicholas570 <nicolas.mercier570@gmail.com>
2020-07-02 12:57:02 +0200

    setup done

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyBNJvgH_a_IAuR87hyZHTdMfi6CabgqsrQ',
+  authDomain: 'react-hooks-authenticati-b1ca8.firebaseapp.com',
+  databaseURL: 'https://react-hooks-authenticati-b1ca8.firebaseio.com',
+  projectId: 'react-hooks-authenticati-b1ca8',
+  storageBucket: 'react-hooks-authenticati-b1ca8.appspot.com',
+  messagingSenderId: '971833550284',
+  appId: '1:971833550284:web:730c41f201616a0a886969',
+});
+
+export default app;
